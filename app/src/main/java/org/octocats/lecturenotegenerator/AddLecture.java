e16404f9027434d20d2d5ee3b05e50a459c0f066
nisargkolhe <nisargkolhe@gmail.com>
2016-10-08 09:21:13 -0400

    +AddLecture

---
+++ app/src/main/java/org/octocats/lecturenotegenerator/AddLecture.java
@@ -0,0 +1,16 @@
+package org.octocats.lecturenotegenerator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nisarg on 8/10/16.
+ */
+public class AddLecture extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_lec);
+
+    }
+}
--- app/src/main/java/org/octocats/lecturenotegenerator/MainActivity.java
+++ app/src/main/java/org/octocats/lecturenotegenerator/MainActivity.java
@@ -1,18 +1,18 @@
 package org.octocats.lecturenotegenerator;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -63,8 +63,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, AddLecture.class);
+                ActivityCompat.startActivity(MainActivity.this, intent, null);
             }
         });
     }
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         @TargetApi(Build.VERSION_CODES.LOLLIPOP)
         @Override
         public void onItemClick(View v, int position) {
-            Log.e(TAG, "clicked "+position);
+
         }
 
     };
