188f793d00bdfe6b2aca6ce09c6339a1661330aa
nisargkolhe <nisargkolhe@gmail.com>
2016-10-08 10:21:00 -0400

    -Moved code from camera to AddLecture

--- app/src/main/java/org/octocats/lecturenotegenerator/AddLecture.java
+++ app/src/main/java/org/octocats/lecturenotegenerator/AddLecture.java
@@ -1,16 +1,37 @@
 package org.octocats.lecturenotegenerator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 /**
  * Created by nisarg on 8/10/16.
  */
 public class AddLecture extends Activity {
+
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_lec);
+
+        ImageButton uploadBtn = (ImageButton) findViewById(R.id.upload);
+        ImageButton recordBtn = (ImageButton) findViewById(R.id.record);
+
+        recordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+                }
+            }
+        });
 
     }
 }
