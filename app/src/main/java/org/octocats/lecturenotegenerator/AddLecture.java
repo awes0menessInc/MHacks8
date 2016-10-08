a3bf4408b94555cfad315f483d49e26148a9e419
Utkarsh <utkjain@gmail.com>
2016-10-08 17:43:37 -0400

    Video record and store working

--- app/src/main/java/org/octocats/lecturenotegenerator/AddLecture.java
+++ app/src/main/java/org/octocats/lecturenotegenerator/AddLecture.java
@@ -2,10 +2,12 @@ package org.octocats.lecturenotegenerator;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.VideoView;
 
 
 /**
@@ -23,14 +25,22 @@ public class AddLecture extends Activity {
         ImageButton uploadBtn = (ImageButton) findViewById(R.id.upload);
         ImageButton recordBtn = (ImageButton) findViewById(R.id.record);
 
-        recordBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        recordBtn.setOnClickListener(new View.OnClickListener()
+        {
+            String videoPath;
+            public void onClick(View view)
+            {
+                //User can take a video from here
+                //after accepting, the video is stored in the dcim of the user's local storage
+                //videoPath String stores the path of the video, which can be accessed in the clicklistener method.
                 Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
                 if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+                    videoPath = takeVideoIntent.getData().getPath();
                 }
             }
+
+
         });
 
     }
