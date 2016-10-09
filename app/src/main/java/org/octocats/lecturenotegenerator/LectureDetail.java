14b0199e3631f66945d39ace61b646c6b45d2af0
nisargkolhe <nisargkolhe@gmail.com>
2016-10-09 10:30:03 -0400

    -something

--- app/src/main/java/org/octocats/lecturenotegenerator/LectureDetail.java
+++ app/src/main/java/org/octocats/lecturenotegenerator/LectureDetail.java
@@ -9,11 +9,18 @@ import android.util.Log;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 import static android.content.ContentValues.TAG;
 
@@ -27,6 +34,10 @@ public class LectureDetail extends Activity
     private StaggeredGridLayoutManager mStaggeredLayoutManager;
     private SumListAdapter mAdapter;
 
+    ArrayList<String> sums = new ArrayList<>();
+
+    ArrayList<Double> times = new ArrayList<>();
+
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -34,6 +45,9 @@ public class LectureDetail extends Activity
 
         Bundle extras = getIntent().getExtras();
         String value = extras.getString("URI");
+        String contentID = extras.getString("CONTENT_ID");
+
+
         JSONObject textSummary = new JSONObject();
         try {
            textSummary = new JSONObject(extras.getString("TEXT_SUMMARY"));
@@ -45,32 +59,75 @@ public class LectureDetail extends Activity
         vid.setMediaController(new MediaController(this));
         vid.start();
         JSONArray sentences = new JSONArray();
+
+
         try {
             sentences = textSummary.getJSONArray("sentences");
+            sums = new ArrayList<String>();
+            if (sentences != null) {
+                for (int i=0;i<sentences.length();i++){
+                    sums.add(sentences.getString(i));
+
+
+                    AsyncHttpClient client = new AsyncHttpClient();
+                    JSONObject jsonParam = new JSONObject();
+
+                    JSONObject filter = new JSONObject();
+                    filter.put("Nmax", 10);
+                    filter.put("Pmin", 0.55);
+                    jsonParam.put("action", "object_search");
+                    jsonParam.put("userID", "1476010302-f9f306e3-773b-4252-832e-dd09172f02f4-1946553840890124384591255084455");
+                    jsonParam.put("contentID", contentID);
+                    jsonParam.put("query", sentences.getString(i));
+                    jsonParam.put("filter", filter);
+                    jsonParam.put("sort", "time");
+                    StringEntity entity = new StringEntity(jsonParam.toString());
+
+                    client.post(getApplicationContext(), "http://api.deepgram.com/", entity, "application/json", new JsonHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                            try {
+                                Log.e(TAG,""+response.toString());
+                                addToTimes(response.getDouble("startTime"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, String res, Throwable t) {
+                            // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                            Log.e(TAG, "" + statusCode);
+                            Log.e(TAG, res.toString());
+
+                        }
+                    });
+
+
+                }
+            }
         } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
 
 
-        ArrayList<String> sums = new ArrayList<String>();
-        if (sentences != null) {
-            for (int i=0;i<sentences.length();i++){
-                try {
-                    sums.add(sentences.getString(i));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+
 
         mRecyclerView = (RecyclerView) findViewById(R.id.sumList);
         mStaggeredLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(mStaggeredLayoutManager);
-        mAdapter = new SumListAdapter(sums, new ArrayList<String>());
+        mAdapter = new SumListAdapter(sums, times);
         mRecyclerView.setAdapter(mAdapter);
 
 
 
+    }
+
+
+    public void addToTimes(double d){
+        times.add(d);
     }
 
 }
